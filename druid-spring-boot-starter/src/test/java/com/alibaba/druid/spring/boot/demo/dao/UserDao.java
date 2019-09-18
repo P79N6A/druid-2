60747d4ba840f7e303ef61a625af931e71a1793c
linrongwu <973218670@qq.com>
2019-09-18 20:13:46 +0800

    druid spring boot

--- druid-spring-boot-starter/src/test/java/com/alibaba/druid/spring/boot/demo/dao/UserDao.java
+++ druid-spring-boot-starter/src/test/java/com/alibaba/druid/spring/boot/demo/dao/UserDao.java
@@ -4,5 +4,7 @@ package com.alibaba.druid.spring.boot.demo.dao;
 import com.alibaba.druid.spring.boot.demo.model.User;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserDao extends CrudRepository<User, Long> {}
+public interface UserDao extends CrudRepository<User, Long> {
+
+}
 
